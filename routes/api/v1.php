5f603380050eb7caa6fbd82a55316e79f689a2cc
rizkhal <lamaaurizkhal@gmail.com>
2022-10-08 13:53:20 +0800

    .

--- routes/api/v1.php
+++ routes/api/v1.php
@@ -4,5 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\SuggestionJsonController;
 
 Route::prefix('v1')->group(function () {
+    // 
     Route::post('suggestions', SuggestionJsonController::class);
 });
