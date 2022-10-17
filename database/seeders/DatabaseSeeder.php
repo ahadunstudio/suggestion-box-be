f0edeb19dad970c7eaca6f7b8d969c7d895396d4
rizkhal <lamaaurizkhal@gmail.com>
2022-10-17 21:59:57 +0800

    .

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,8 +17,8 @@ class DatabaseSeeder extends Seeder
         \App\Models\User::factory(10)->create();
 
         \App\Models\User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@mail.com',
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
         ]);
     }
 }
